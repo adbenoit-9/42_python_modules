045815d940afd0408c3d59fb62c101b7afc84169
Adeline BENOIT <adbenoit@e1r8p12.clusters.42paris.fr>
2022-05-16 17:11:54 +0200

    ex03 ok

---
+++ module00/ex03/count.py
@@ -0,0 +1,30 @@
+import string
+
+def text_analyzer(*args, **kwargs):
+    if len(args) > 1:
+        print("ERROR")
+        return
+    elif len(args) < 1:
+        text = input("What is the text to analyse? ")
+    else:
+        text = args[0]
+    uppercase = 0
+    lowercase = 0
+    punct = 0
+    space = 0
+    for c in text:
+        if c.isupper():
+            uppercase += 1;
+        elif c.islower():
+            lowercase += 1;
+        elif c == " ":
+            space += 1
+        else:
+            for i in string.punctuation:
+                if c == i:
+                    punct += 1
+    print("the text contains " + str(len(text)) + " characters:\n"
+    + "- " + str(uppercase) + " upper letters\n"
+    + "- " + str(lowercase) + " lower letters\n"
+    + "- " + str(punct) + " punctuation marks\n"
+    + "- " + str(space) + " spaces\n")

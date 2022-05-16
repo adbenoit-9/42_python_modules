6ac822214c559507a663e40e466f9c96102f2343
Adeline BENOIT <adbenoit@e1r8p12.clusters.42paris.fr>
2022-05-16 18:29:24 +0200

    ex05 ok

--- module00/ex05/kata01.py
+++ module00/ex05/kata01.py
@@ -3,3 +3,8 @@ languages = {
     'Ruby': 'Yukihiro Matsumoto',
     'PHP': 'Rasmus Lerdorf',
     }
+
+str = 'Python was created by {Python}\n'
+str += 'Ruby was created by {Ruby}\n'
+str += 'PHP was created by {PHP}'
+print(str.format(**languages))
---
+++ module00/ex05/kata03.py
@@ -0,0 +1,5 @@
+import sys
+
+phrase = "The right format"
+
+sys.stdout.write(phrase.rjust(42, '-'))

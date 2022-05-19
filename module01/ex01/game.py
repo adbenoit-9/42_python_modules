6363737382e860f34d2f04d2b7f5ff320dc1151c
adbenoit-9 <adeline.benoit@hotmail.com>
2022-05-20 01:50:43 +0200

    ex01 fixed

--- module01/ex01/game.py
+++ module01/ex01/game.py
@@ -1,13 +1,14 @@
 class GotCharacter:
-    def __init__(self, first_name):
+    '''A class representing a Got Character'''
+    def __init__(self, first_name=None, is_alive=True):
         self.first_name = first_name
-        self.is_alive = True
+        self.is_alive = is_alive
 
 
 class Stark(GotCharacter):
     '''A class representing the Stark family. \
 Or when bad things happen to good people.'''
-    def __init__(self, name):
+    def __init__(self, first_name=None, is_alive=True):
         GotCharacter.__init__(self, name)
         self.family_name = "Stark"
         self.house_words = "Winter is Coming"

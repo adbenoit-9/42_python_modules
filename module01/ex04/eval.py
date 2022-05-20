79f373d36dfba466777da03ba6cac4e43a72fac2
Adeline BENOIT <adbenoit@e1r10p18.clusters.42paris.fr>
2022-05-20 14:42:02 +0200

    eval fixed

--- module01/ex04/eval.py
+++ module01/ex04/eval.py
@@ -1,16 +1,28 @@
 class Evaluator:
     def zip_evaluate(coefs, words):
-        if len(coefs) != len(words):
+        if isinstance(coefs, list) is False or \
+                isinstance(words, list) is False or \
+                len(coefs) != len(words) or \
+                len(coefs) == 0:
             return -1
         ret = 0
         for coef, word in zip(coefs, words):
+            if isinstance(coef, float) is False or \
+                    isinstance(word, str) is False:
+                return -1
             ret += coef * len(word)
         return ret
 
     def enumerate_evaluate(coefs, words):
-        if len(coefs) != len(words):
+        if isinstance(coefs, list) is False or \
+                isinstance(words, list) is False or \
+                len(coefs) != len(words) or \
+                len(coefs) == 0:
             return -1
         ret = 0
         for i, coef in enumerate(coefs):
+            if isinstance(coef, float) is False or \
+                    isinstance(words[i], str) is False:
+                return -1
             ret += coef * len(words[i])
         return ret

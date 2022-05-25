69e1106dd2a70bcb3e99bc0cc19dbf3034db40aa
Adeline BENOIT <adbenoit@e1r8p10.clusters.42paris.fr>
2022-05-25 19:08:06 +0200

    ex06 ok

--- module04/ex06/MyPlotLib.py
+++ module04/ex06/MyPlotLib.py
@@ -13,38 +13,44 @@ class MyPlotLib:
         if isinstance(data, pd.DataFrame) is False or \
                 isinstance(features, list) is False:
             return None
-        axis = plt.subplots(len(features))[1]
-        for i, elem in enumerate(features):
-            x = data[elem].tolist()
-            axis[i].hist(x, 15, density=False, facecolor='b', alpha=0.75)
-            plt.title(elem)
-            axis[i].grid(True)
-        plt.show()
+        try:
+            axis = plt.subplots(len(features))[1]
+            for i, elem in enumerate(features):
+                x = data[elem].tolist()
+                axis[i].hist(x, 15, density=False, facecolor='b', alpha=0.75)
+                plt.title(elem)
+                axis[i].grid(True)
+            plt.show()
+        except Exception:
+            print('Error: Failed to display historgram.')
 
     def density(self, data, features):
         if isinstance(data, pd.DataFrame) is False or \
                 isinstance(features, list) is False:
             return None
-        axis = plt.subplots()[1]
-        for elem in features:
-            pd.DataFrame(data[elem]).plot(ax=axis, kind='density')
-        plt.show()
+        try:
+            axis = plt.subplots()[1]
+            for elem in features:
+                pd.DataFrame(data[elem]).plot(ax=axis, kind='density')
+            plt.show()
+        except Exception:
+            print('Error: Failed to display density.')
 
     def pair_plot(self, data, features):
         if isinstance(data, pd.DataFrame) is False or \
                 isinstance(features, list) is False:
             return None
-        sb.pairplot(data.filter(items=features))
-        plt.show()
+        try:
+            sb.pairplot(data.filter(items=features))
+            plt.show()
+        except Exception:
+            print('Error: Failed to display pairplot.')
         
 
     def box_plot(self, data, features):
         if isinstance(data, pd.DataFrame) is False or \
                 isinstance(features, list) is False:
             return None
-        my_dict = {}
-        for elem in features:
-            my_dict[elem] = data[elem].tolist()
         try:
             data.boxplot(column=features, grid=False)
             plt.show()

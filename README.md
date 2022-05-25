1f17c439a6764eeaa043a1458ca92a2510e1c63f
Adeline BENOIT <adbenoit@e1r8p10.clusters.42paris.fr>
2022-05-25 19:01:32 +0200

    install_conda

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 ```
 $ git clone https://github.com/adbenoit-9/42_python_modules.git
 $ cd 42_python_modules
+$ ./install_conda.sh
+$ exit
 $ conda activate 42AI-$USER
 ```
 
---
+++ install_conda.sh
@@ -0,0 +1,9 @@
+MYPATH="/goinfre/$USER/miniconda3"
+curl -LO "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+sh Miniconda3-latest-Linux-x86_64.sh -b -p $MYPATH
+$MYPATH/bin/conda init zsh
+$MYPATH/bin/conda config --set auto_activate_base false
+source ~/.zshrc
+conda create --name 42AI-$USER python=3.7 jupyter pandas pycodestyle
+conda info --envs
+Miniconda3-latest-Linux-x86_64.sh
